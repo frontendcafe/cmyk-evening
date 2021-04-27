cf7ceaa1bb5e5ac62b5fb9db8bc1053bea3c5dc9
JoseIgnacioTapia <josignaciot@gmail.com>
2021-04-26 21:40:52 -0300

    Get Info from JSON

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('hola')
+const apiURL = '../data/tour_destinos.json';
+let resultsArray = [];
+
+const countMobile = 3;
+const countTablet = 4;
+const countDesktop = 6;
+
+function createDOMNodes(page) {
+  const currentArray = page === 'homePage' ? resultsArray : allTours;
+}
+
+function updateDOM(page) {
+  createDOMNodes(page);
+  showContent(page);
+}
+
+// Obtener info del JSON
+async function getInfoTours() {
+  try {
+    const response = await fetch(apiURL);
+    resultsArray = await response.json();
+    console.log(resultsArray[0].city.name);
+    updateDOM('homePage');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getInfoTours();
