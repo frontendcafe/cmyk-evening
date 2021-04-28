9cff77c3091c36fd2b185b83571af6ac38278cb9
JoseIgnacioTapia <josignaciot@gmail.com>
2021-04-28 16:50:04 -0300

    Mobile random cards

--- js/app.js
+++ js/app.js
@@ -1,29 +1,61 @@
 const apiURL = '../data/tour_destinos.json';
 let resultsArray = [];
 
-const countMobile = 3;
-const countTablet = 4;
-const countDesktop = 6;
+const cardsSection = document.querySelector('#cards-section');
 
-function createDOMNodes(page) {
-  const currentArray = page === 'homePage' ? resultsArray : allTours;
-}
-
-function updateDOM(page) {
-  createDOMNodes(page);
-  showContent(page);
-}
+const mobile = 3;
+const tablet = 4;
+const desktop = 6;
 
 // Obtener info del JSON
 async function getInfoTours() {
   try {
     const response = await fetch(apiURL);
     resultsArray = await response.json();
-    console.log(resultsArray[0].city.name);
-    updateDOM('homePage');
+
+    displayCards(mobile);
   } catch (error) {
     console.log(error);
   }
 }
+
+function displayCards() {
+  const sizeVW = window.innerWidth;
+
+  // if (sizeVW < 769) {
+  //  const screen = mobile;
+  // } else if(sizeVW < 1137) {
+  //   const screen = tablet;
+  // } else {
+  //    const screen = desktop;
+  // }
+
+  for (let i = 0; i < screen; i++) {
+    const randomI = Math.floor(Math.random() * resultsArray.length);
+    console.log(randomI);
+    console.log(resultsArray);
+    const city = resultsArray[randomI].city.name;
+    const country = resultsArray[randomI].city.country;
+    const price = resultsArray[randomI].priceUsd;
+    imgFile = city.split(' ').join('-');
+    console.log(city);
+
+    cardsSection.innerHTML += `
+      <div class="tour-card tour-card-${i + 1}" 
+        style="background:#fff url(../assets/imgs/cards/${imgFile}.jpeg) no-repeat center;">
+        <div class="card-content">
+          <div class="first-content">
+            <span class="tour-price">$${price}</span>
+          </div>
+          <div class="second-content">
+            <h6 class="card-country">${country}</h6>
+            <h3 class="card-place">${city}</h3>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+// CAMBIAR TODAS LAS IMAGENES PARA LA CARPETA IMGS/CARDS Y IMPLEMENTAR UN RANDOM PARA TOMAR 3 VALORES DEL ARRAY resultsArray
 
 getInfoTours();
